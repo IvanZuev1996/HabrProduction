0d3b80621b8580ac16aca19bbfa910f0595fd2af
IvanZuev1996 <93051173+IvanZuev1996@users.noreply.github.com>
2022-09-17 22:14:24 +0300

    added a react-router-dom and lazy imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
     return (
         <div className="app">
-            fewfewpjfpew
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О Сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPageAsync />} />
+                    <Route path="/about" element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
