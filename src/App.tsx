f29daad54f626e3a90254303e34d5a49cd394599
IvanZuev1996 <93051173+IvanZuev1996@users.noreply.github.com>
2022-09-18 01:17:32 +0300

    added a toggle a themes styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О Сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
