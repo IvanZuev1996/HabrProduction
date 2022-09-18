b3ed4dd00e0e79b176479f538b5dd18f444a0050
IvanZuev1996 <93051173+IvanZuev1996@users.noreply.github.com>
2022-09-18 21:03:33 +0300

    added a new files structure and FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,22 +2,23 @@ import { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
+import { AboutPage } from 'pages/AboutPage';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { MainPage } from 'pages/MainPage';
+import { classNames } from 'shared/lib/helpers/classNames';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toggle theme</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О Сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path="/" element={<MainPageAsync />} />
-                    <Route path="/about" element={<AboutPageAsync />} />
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/about" element={<AboutPage />} />
                 </Routes>
             </Suspense>
         </div>
