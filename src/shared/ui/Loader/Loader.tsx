2ac2426fb9d90f176038836a04cc438a8491ab9f
IvanZuev1996 <93051173+IvanZuev1996@users.noreply.github.com>
2022-09-30 00:30:04 +0300

    add not found page, add pageLoader component

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/helpers/classNames';
+import cls from './Loader.module.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames(cls['lds-ellipsis'], {}, [className])}>
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+);
