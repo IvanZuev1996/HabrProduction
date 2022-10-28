2c49308d142fe336b4d894d224444247ca806ec3
IvanZuev1996 <93051173+IvanZuev1996@users.noreply.github.com>
2022-10-29 01:16:25 +0300

    add validation for profileCard, fix all screenshots

--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -1,6 +1,18 @@
 import { Country } from 'entities/Country';
 import { Currency } from 'entities/Currency';
 
+export enum ValidateProfileError {
+    INCORRECT_FIRSTNAME = 'INCORRECT_FIRSTNAME',
+    INCORRECT_LASTNAME = 'INCORRECT_LASTNAME',
+    INCORRECT_AGE = 'INCORRECT_AGE',
+    INCORRECT_USERNAME = 'INCORRECT_USERNAME',
+    INCORRECT_COUNTRY = 'INCORRECT_COUNTRY',
+    INCORRECT_CURRENCY = 'INCORRECT_CURRENCY',
+    INCORRECT_CITY = 'INCORRECT_CITY',
+    NO_DATA = 'NO_DATA',
+    SERVER_ERROR = 'SERVER_ERROR'
+}
+
 export interface Profile {
     firstname?: string;
     lastname?: string;
@@ -18,4 +30,5 @@ export interface ProfileSchema {
     isLoading: boolean;
     error?: string;
     readonly: boolean;
+    validateError?: ValidateProfileError[];
 }
