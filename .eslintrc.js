1e2a545e2043e22fab69951580fb93840c632766
Ivan Zuev <ivan-zuev-97@mail.ru>
2023-08-17 19:10:26 +0300

    update custom eslint-plugin, add aliases support

--- .eslintrc.js
+++ .eslintrc.js
@@ -77,7 +77,7 @@ module.exports = {
         'no-param-reassign': 'off',
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
-        'babun4ek-fsd-plugin/path-checker': 'error'
+        'babun4ek-fsd-plugin/path-checker': ['error', {alias: '@'}]
     },
     globals: {
         __IS_DEV__: true,
